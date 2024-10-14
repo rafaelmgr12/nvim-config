303b79eb82ceffb7c3bfaf2b1cf29b96fadafd89
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-10-14 14:24:30 -0300

    Add new plugins: toggleterm.nvim, gitignore.nvim, and render-markdown.nvim
    - Added toggleterm.nvim for floating terminal support with <c-\> as the toggle shortcut.
    - Added gitignore.nvim for easier handling of .gitignore files.
    - Added render-markdown.nvim with markdown rendering support and dependencies on nvim-treesitter and mini.nvim.

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -55,5 +55,30 @@ local plugins = {
       vim.cmd [[silent! GoInstallDeps]]
     end,
   },
+  {
+    "akinsho/toggleterm.nvim",
+    version = '*',
+    config = function()
+      require("toggleterm").setup{
+        open_mapping = [[<c-\>]],  -- Atalho para abrir/fechar o terminal
+        direction = 'float',       -- Terminal flutuante (pode ser 'horizontal' ou 'vertical')
+      }
+    end
+  },
+  {
+    "wintermute-cell/gitignore.nvim", -- Adicionando o plugin gitignore.nvim
+    config = function()
+      require("gitignore")
+    end
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+  
+},
 }
 return plugins
